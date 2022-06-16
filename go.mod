0c675387d7eba64543a0854975a9d61c68ab6677
Nathan Wehr <nathan@wehrholdings.com>
2022-06-15 21:01:44 -0400

    Using unix.Open instead of os.OpenFile

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/creltek/taptun
+
+go 1.18
+
+require golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
--- taptun_linux.go
+++ taptun_linux.go
@@ -14,6 +14,37 @@ type ifreq struct {
 }
 
 func createInterface(flags uint16, name string) (string, *os.File, error) {
+	nfd, err := unix.Open("/dev/net/tun", os.O_RDWR, 0)
+    if err = nil {
+        return "", nil, err
+    }
+
+	var ifr [unix.IFNAMSIZ + 64]byte
+	copy(ifr[:], []byte(name))
+
+	*(*uint16)(unsafe.Pointer(&ifr[unix.IFNAMSIZ])) = flags
+
+	_, _, errno := unix.Syscall(
+		unix.SYS_IOCTL,
+		uintptr(nfd),
+		uintptr(unix.TUNSETIFF),
+		uintptr(unsafe.Pointer(&ifr[0])),
+	)
+	if errno != 0 {
+        return "", nil fmt.Errorf("ioctl errno: %d", errno)
+	}
+
+	if err = unix.SetNonblock(nfd, true); err != nil {
+        return "", nil, err
+    }
+
+	fd := os.NewFile(uintptr(nfd), tunDevice)
+
+    return string(ifr[:unix.IFNAMSIZ]), fd, nil
+}
+
+/*
+func createInterface(flags uint16, name string) (string, *os.File, error) {
 	// Last byte of name must be nil for C string, so name must be
 	// short enough to allow that
 	if len(name) > syscall.IFNAMSIZ-1 {
@@ -39,6 +70,7 @@ func createInterface(flags uint16, name string) (string, *os.File, error) {
 	}
 	return cstringToGoString(ifr.name[:]), f, nil
 }
+*/
 
 func destroyInterface(name string) error {
 	return nil
